f7e1f08bfd9401c0c8425d4db11988ab91beeacc
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-02-01 19:22:44 +0700

    using CQRS

--- src/ProduceFlow.API/Controllers/AssetsController.cs
+++ src/ProduceFlow.API/Controllers/AssetsController.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 using ProduceFlow.Application.DTOs.Assets;
 using ProduceFlow.Application.Services;
 using ProduceFlow.Domain.Entities;
+using ProduceFlow.Application.UseCases.Assets.Commands.CreateAsset;
+using ProduceFlow.Application.UseCases.Assets.Commands.DeleteAsset;
+using ProduceFlow.Application.UseCases.Assets.Commands.UpdateAsset;
+using ProduceFlow.Application.UseCases.Assets.Queries.GetAllAssets;
+using ProduceFlow.Application.UseCases.Assets.Queries.GetAssetById;
+using MediatR;
 
 namespace ProduceFlow.API.Controllers;
 
@@ -9,69 +15,52 @@ namespace ProduceFlow.API.Controllers;
 [Route("api/[controller]")]
 public class AssetsController : ControllerBase
 {
-    private readonly AssetService _service;
+    private readonly IMediator _mediator;
 
-    public AssetsController(AssetService service)
+    public AssetsController(IMediator mediator)
     {
-        _service = service;
+        _mediator = mediator;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var assets = await _service.GetAllAssetsAsync();
+        var assets = await _mediator.Send(new GetAllAssetsQuery());
         return Ok(assets);
     }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateAssetRequest request)
     {
-            var result = await _service.CreateAssetAsync(request);
-            return CreatedAtAction(nameof(GetAll), new { }, result);
-        
+        var command = new CreateAssetCommand(
+            request.Name,
+            request.Description,
+            request.Price,
+            request.Quantity,
+            request.Status
+        );
+        var result = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
     }
 
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetById(int id)
     {
-        var result = await _service.GetAssetByIdAsync(id);
+        var result = await _mediator.Send(new GetAssetByIdQuery(id));
         return Ok(result);
     }
 
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete(int id)
     {
-        try
-        {
-            await _service.DeleteAssetAsync(id);
-            return NoContent();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        await _mediator.Send(new DeleteAssetCommand(id));
+        return NoContent();
     }
 
     [HttpPut("{id:int}")]
     public async Task<IActionResult> Update(int id, [FromBody] UpdateAssetRequest request)
     {
-        try
-        {
-            await _service.UpdateAssetAsync(id, request);
-            return NoContent();
-        }
-        catch (KeyNotFoundException)
-        {
-
-            return NotFound();
-        }
-        catch (FluentValidation.ValidationException ex)
-        {
-            return BadRequest(ex.Errors);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        await _mediator.Send(new UpdateAssetCommand(id, request.Name, request.Description, request.Price, request.Quantity, request.Status));
+        return NoContent();
     }
 }
